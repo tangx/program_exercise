6d4398d6c4cdaefae86fb8e470bab21990b2f061
HsinTang <uyinn@live.com>
2016-08-25 14:49:08 +0800

    添加 random.choice 和 string模块
    1. 使用生成器重复调用random.choice 使得密码字符更随机
    2. 使用string.letters, string.digits, string.lowercase, string.uppercase，
    更方便的建立密码字符池。

--- show_me_the_code/subject_0001_class.py
+++ show_me_the_code/subject_0001_class.py
@@ -41,7 +41,12 @@ class PromoteCodeGenerator:
 
         return self.code_pool
 
-    def get_generator_code(self, lower=True, upper=False, digit=False, symbol=False, user_define=''):
+    def get_code_with_random_sample(self, lower=True, upper=False, digit=False, symbol=False, user_define=''):
+        '''
+            random.sample(string,length)：
+                1. 只能在string当中选择length个元素，每个有且只有一次被选中。
+                2. length 的值不能大于 len(string)
+        '''
         self.get_code_pool(lower, upper, digit, symbol, user_define)
 
         if self.code_length < len(self.code_pool):
@@ -49,9 +54,31 @@ class PromoteCodeGenerator:
         else:
             print "too long"
 
+    def get_code_with_random_choice(self, lower=True, upper=False, digit=False, symbol=False, user_define=''):
+        '''
+            1. 通过string生成 code_pool
+            2. random.choice(char) 选择一个元素，重复 N 次
+            3. 通过 random.choice 方法，由于允许字符重复出现，生成的密码更随机，
+
+        '''
+
+        import string
+        self.code_pool += user_define
+        if lower is True:
+            self.code_pool += string.lowercase
+        if upper is True:
+            self.code_pool += string.uppercase
+        if digit is True:
+            self.code_pool += string.digits
+        if symbol is True:
+            self.code_pool += self.symbol_c
+
+        print ''.join([random.choice(self.code_pool) for i in xrange(self.code_length)])
+
 
 if __name__ == '__main__':
     iphone_gen = PromoteCodeGenerator()
     iphone_gen.gen_times = 1
     iphone_gen.code_length = 20
-    iphone_gen.get_generator_code()
+    # iphone_gen.get_generator_code()
+    iphone_gen.get_code_with_random_choice(True, True, True, False, '_+=')
